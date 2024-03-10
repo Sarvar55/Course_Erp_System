c1a7f586e2422a97cda095ffd03322d7c083fa0b
Sarvar55 <sarvar.musazade@gmail.com>
2024-03-10 15:56:45 +0300

    Refactor: Implement OTP jwt with secret key

--- src/main/java/com/erp/erpbackend/models/porperties/otp/OTPJwtData.java
+++ src/main/java/com/erp/erpbackend/models/porperties/otp/OTPJwtData.java
@@ -1,4 +1,11 @@
 package com.erp.erpbackend.models.porperties.otp;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class OTPJwtData {
+    String secretKey;
 }
--- src/main/java/com/erp/erpbackend/services/otp/OTPProceedTokenManager.java
+++ src/main/java/com/erp/erpbackend/services/otp/OTPProceedTokenManager.java
@@ -2,16 +2,20 @@ package com.erp.erpbackend.services.otp;
 
 import com.erp.erpbackend.constants.TokenConstants;
 import com.erp.erpbackend.models.mybatis.user.User;
+import com.erp.erpbackend.models.porperties.otp.OTPProperties;
 import com.erp.erpbackend.services.base.TokenGenerator;
 import com.erp.erpbackend.services.base.TokenReader;
 import com.erp.erpbackend.utils.PublicPrivateKeyUtils;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
 
 @Component
@@ -19,25 +23,32 @@ import java.util.Date;
 @Slf4j
 public class OTPProceedTokenManager implements TokenGenerator<User>, TokenReader<Claims> {
 
+    private final OTPProperties otpProperties;
+
     @Override
     public String generate(User obj) {
         Claims claims = Jwts.claims();
         claims.put(TokenConstants.EMAIL, obj.getEmail());
 
         Date now = new Date();
-        Date exp = new Date(now.getTime() + securityProperties.getJwt().getAccessTokenValidityTime());
+        Date exp = new Date(now.getTime() + otpProperties.getValidityTime());
 
         return Jwts.builder()
                 .setClaims(claims)
                 .setExpiration(exp)
                 .setIssuedAt(now)
                 .setSubject(String.valueOf(obj.getId()))
-                .signWith(PublicPrivateKeyUtils.getPrivateKey(), SignatureAlgorithm.RS256)
+                .signWith(getSigningKey(), SignatureAlgorithm.HS512)
                 .compact();
     }
 
     @Override
     public Claims read(String token) {
         return null;
+    }
+
+    private Key getSigningKey() {
+        byte[] keyBytes = otpProperties.getOtpJwtData().getSecretKey().getBytes(StandardCharsets.UTF_8);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
